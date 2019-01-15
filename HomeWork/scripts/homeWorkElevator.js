de5b2f4f6dcedef901b8f826e38da75cf87cc7a9
melenevskayan <45152728+melenevskayan@users.noreply.github.com>
2019-01-15 21:38:15 +0200

    Add files via upload

---
+++ HomeWork/scripts/homeWorkElevator.js
@@ -0,0 +1,37 @@
+﻿function Elevator(newElevator){
+	var e 		       = this;
+		e.elevator     = $(newElevator);
+		console.log('e.elevator now is: ', e.elevator);
+		e.btn          = e.elevator.find('.btn');
+		e.messagePlace = e.elevator.find('.message');
+		console.log("my message place:", e.messagePlace);
+		e.currentFloor = 1;
+		
+		
+
+
+	e.movement = function(){
+		var targetFloor = $(this).text();
+		alert(targetFloor);
+		if(targetFloor > e.currentFloor){
+			message = 'Elevator is going up to ' + targetFloor + ' floor';
+		}
+		else if(targetFloor < e.currentFloor){
+			message = 'Elevator is going down to ' + targetFloor + ' floor';
+		}
+		else if(targetFloor == e.currentFloor){
+			message = "You're already on the needed floor";
+		}
+		else if(targetFloor == "Stop"){
+			message = "Elevator is stopped";
+		}
+		e.currentFloor = targetFloor;
+		e.messagePlace.text(message);
+	}
+	e.btn.click(e.movement);
+}
+
+
+
+
+
---
+++ HomeWork/scripts/plusMinusValue.js
@@ -0,0 +1,46 @@
+﻿function InputGroup(sSelector){
+	var i             = this;
+	i.inputGroup      = $(sSelector);
+	console.log(i.inputGroup);
+	i.btnMinus        = i.inputGroup.find('.button-minus');
+	i.number          = i.inputGroup.find('.quantity-field');
+	i.btnPlus         = i.inputGroup.find('.button-plus');
+	i.messagePlace    = i.inputGroup.find('.message');
+	i.currentNumber   = i.number.val();
+	console.log("my current number is:", i.currentNumber);
+
+
+	i.reduceGuestNumber = function(){
+		i.currentNumber > 1? (i.currentNumber --, 
+		i.number.val(i.currentNumber)) : 
+		alert("Minimum one guest is required");
+		/*if(i.currentNumber > 1){
+			i.currentNumber --;
+			i.number.val(i.currentNumber);
+			console.log("i.number is now:", i.number.val());	
+		}
+		else{
+			alert("Minimum one guest is required");
+		}*/
+	}
+
+	i.increaseGuestNumber = function(){
+		i.currentNumber ++;
+		i.number.val(i.currentNumber);
+		console.log("i.number is now:", i.number.val());
+	}
+
+	i.number.on('input',function(){ 
+    i.currentNumber = $(this).val()
+});
+
+	/*$('#someInput').on('input', function() { 
+    $(this).val() // get the current value of the input field. 
+});*/ 
+	
+
+	i.btnMinus.click(i.reduceGuestNumber);
+	i.btnPlus.click(i.increaseGuestNumber);
+}
+
+
